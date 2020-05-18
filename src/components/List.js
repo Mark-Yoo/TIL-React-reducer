1dea03a272c3913d1ecfaa0567de4e436c63d168
Mark Yoo <yjinhyuk@gmail.com>
2020-05-18 15:19:57 +0900

    Feat: React context, reducer practice with json-server

---
+++ src/components/List.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const List = ({ book, deletePhone }) => {
+  return (
+    <ul>
+      {book.map((b) => {
+        console.log(b.id);
+        return (
+          <li key={b.id}>
+            {b.name + ":" + b.phone}
+            <button onClick={() => deletePhone(b.id)}>삭제</button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default List;
